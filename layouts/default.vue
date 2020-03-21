95938bbd30103cf2e7e4264c4a03b3ef8e7bb95a
Stephen Martin <sudormminusrf@gmail.com>
2020-03-21 17:34:26 +0000

    Logout of dashboard

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
 <div class="dashboard__grid-container">
-  <header class="dashboard__header border-b border-grey-200 flex ml-auto px-6 items-center">
-    <i class="far fa-bell mr-4 text-xl"></i>
-    <span class="font-semibold">{{ user.name }}</span>
+  <header class="dashboard__header border-b border-grey-200 flex flex-row  px-6 items-center">
+    <i class="far fa-bell mr-4 text-xl  ml-auto"></i>
+    <div>
+      <span v-click-outside="hideUserMenu" @click="showUserMenu = !showUserMenu" class="font-semibold relative cursor-pointer select-none">{{ user.name }}</span>
+      <div v-if="showUserMenu" class="mt-2 py-2 w-48 bg-white border border-grey-300 rounded-lg shadow-xl absolute mt-2 right-0 mr-2">
+        <span @click="logout()" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500">Sign out</span>
+      </div>
+    </div>
   </header>
   <aside class="dashboard__sidenav border-r border-grey-200">
     <div class="dashboard__sidenav-logo">
@@ -26,10 +31,21 @@
 <script>
   import Sidebar from '@/components/Page/Sidebar';
   import AddVendorModal from '@/components/Modals/AddVendorAccount';
-import { mapActions, mapGetters } from 'vuex';
+  import ClickOutside from 'vue-click-outside'
+  import { mapActions, mapGetters } from 'vuex';
 
   export default {
     components: { AddVendorModal, Sidebar },
+
+    data(){
+      return {
+        showUserMenu: false
+      }
+    },
+
+    directives: {
+      ClickOutside
+    },
 
     created() {
       if (this.$auth.loggedIn && ! this.activeVendor && this.user.vendors.data[0]) {        
@@ -47,7 +63,19 @@ import { mapActions, mapGetters } from 'vuex';
     },
 
     methods: {
-      ...mapActions({setActiveVendor: 'vendors/setActive'})
+      ...mapActions({setActiveVendor: 'vendors/setActive'}),
+      
+      hideUserMenu() {
+        this.showUserMenu = false;
+      },
+
+      logout() {
+        this.$auth.logout().then(() => {
+          this.$toast.show('You have logged out')
+          this.$nuxt.$emit('logout')
+          this.$router.push('/login')
+        })
+      },
     },
   }
 </script>
