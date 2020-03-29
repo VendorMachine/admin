a3ff4edb744023d657a3753e362dc9f85c1db39d
Stephen Martin <sudormminusrf@gmail.com>
2020-03-29 14:20:46 +0100

    Basic pagination working

--- pages/vendors/index.vue
+++ pages/vendors/index.vue
@@ -2,24 +2,25 @@
   <div>
     <h1>Vendors</h1>
     <div>
-      <div class="flex">
+      <div class="flex">  
         <button class="ml-auto btn btn--primary" @click="newVendor"><i class="fa fa-plus"></i> New</button>
       </div>
       <AddVendorModal />
-      <section class="flex flex-wrap p-4 " v-if="! $fetchState.pending">
-       <div :key="vendor.id" v-for="vendor in vendors" class="mt-2 w-1/2 p-1">
-        <nuxt-link class="" :to="`/vendors/${vendor.id}/edit`">
-          <div class="card p-4 no-underline">
-            <div class="card__header">
-              <span class="card__status border-green-400"></span>
-              <h2 class="font-bold text-lg text-grey-600">{{ vendor.name }}</h2>
+        <Pagination v-if="pagination.total_pages > 1" class="w-full mt-6 mx-auto" :page="pagination.current_page" :per-page="pagination.per_page" :records="pagination.total" @paginate="setPage"></Pagination>
+      <section class="flex flex-wrap p-4 " v-if="!$fetchState.pending && !busyPaging">
+        <div :key="vendor.id" v-for="vendor in vendors" class="mt-2 w-1/2 p-1">
+          <nuxt-link class="" :to="`/vendors/${vendor.id}/edit`">
+            <div class="card p-4 no-underline">
+              <div class="card__header">
+                <span class="card__status border-green-400"></span>
+                <h2 class="font-bold text-lg text-grey-600">{{ vendor.name }}</h2>
+              </div>
+              <div class="card__body">
+                {{vendor}}
+              </div>
             </div>
-            <div class="card__body">
-              {{vendor}}
-            </div>
-          </div>
-        </nuxt-link>
-       </div>
+          </nuxt-link>
+        </div>
       </section>
       <section v-else>
         Loading
@@ -30,18 +31,34 @@
 
 <script>
   import AddVendorModal from '@/components/Vendors/AddVendorAccountModal';
-  import {mapActions, mapState} from 'vuex';
+  import { mapActions, mapState } from 'vuex';
+  import Pagination from 'vue-pagination-2';
+
   export default {
     components: {
-      AddVendorModal
+      AddVendorModal, Pagination
+    },
+
+    data(){
+      return {
+        busyPaging: false
+      }
     },
 
     methods: {
       ...mapActions({
-        vendorsList: 'vendors/index'
+        vendorsList: 'vendors/index',
       }),
       newVendor() {
         return this.$modal.show('add-vendor');
+      },
+      setPage(page) {
+        if (this.busyPaging) {return false}
+        this.busyPaging = true
+        return this.vendorsList({ page })
+        .finally(() => {
+          this.busyPaging = false
+        })
       }
     },
 
@@ -55,6 +72,9 @@
       }),
       vendors() {
         return this.$store.state.vendors.list;
+      },
+      pagination() {
+        return this.$store.state.vendors.meta.pagination ? this.$store.state.vendors.meta.pagination : {}
       }
     },
   }
