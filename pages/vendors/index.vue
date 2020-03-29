5176f08c99de32ff44cd5965a4c1f180c2add10e
Stephen Martin <sudormminusrf@gmail.com>
2020-03-29 16:17:58 +0100

    Added vendor search by name

--- pages/vendors/index.vue
+++ pages/vendors/index.vue
@@ -3,11 +3,12 @@
     <h1>Vendors</h1>
     <div>
       <div class="flex">  
+        <input @input="search()" v-model="searchTerm" class="input__search w-1/3" placeholder="Search">
         <button class="ml-auto btn btn--primary" @click="newVendor"><i class="fa fa-plus"></i> New</button>
       </div>
       <AddVendorModal />
         <Pagination v-if="pagination.total_pages > 1" class="w-full mt-6 mx-auto" :page="pagination.current_page" :per-page="pagination.per_page" :records="pagination.total" @paginate="setPage"></Pagination>
-      <section class="flex flex-wrap p-4 " v-if="!$fetchState.pending && !busyPaging">
+      <section class="flex flex-wrap p-4 " v-if="!$fetchState.pending && !busyResults">
         <div :key="vendor.id" v-for="vendor in vendors" class="mt-2 w-1/2 p-1">
           <nuxt-link class="" :to="`/vendors/${vendor.id}/edit`">
             <div class="card p-4 no-underline">
@@ -41,7 +42,8 @@
 
     data(){
       return {
-        busyPaging: false
+        searchTerm: null,
+        busyResults: false
       }
     },
 
@@ -53,11 +55,18 @@
         return this.$modal.show('add-vendor');
       },
       setPage(page) {
-        if (this.busyPaging) {return false}
-        this.busyPaging = true
-        return this.vendorsList({ page })
+        if (this.busyResults) {return false}
+        this.busyResults = true
+        return this.vendorsList({ page, s: this.searchTerm })
         .finally(() => {
-          this.busyPaging = false
+          this.busyResults = false
+        })
+      },
+      search() {
+        if (this.busyResults) {return false}
+        return this.vendorsList({ page: 1, s: this.searchTerm })
+        .finally(() => {
+          this.busyResults = false
         })
       }
     },
